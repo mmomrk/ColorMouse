9ca100e24bbdd2e20445e56067a8989910ef0403
mmomrk <mr.ak-47@rambler.ru>
2013-08-24 06:39:24 -0700

    added .idea

---
+++ src/PixelHunter/GroupVariables.java
@@ -0,0 +1,10 @@
+package PixelHunter;
+
+
+/**
+ * User: mrk
+ * Date: 8/24/13; Time: 5:53 AM
+ */
+public class GroupVariables
+{
+}
---
+++ src/PixelHunter/SecondaryLivingCreature.java
@@ -0,0 +1,8 @@
+package PixelHunter;
+/**
+ * User: mrk
+ * Date: 8/24/13; Time: 6:01 AM
+ */
+public class SecondaryLivingCreature
+{
+}
