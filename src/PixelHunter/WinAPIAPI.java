2ea80d4b0e908e30c9ad5e737da6e0159f92647b
arty_57 <artytarasov@yandex.ru>
2013-08-24 20:19:08 +0400

    Added info frame and tooltip classes. Modify winapiapi to wrk with it in showMessage and tooltip methods

--- src/PixelHunter/WinAPIAPI.java
+++ src/PixelHunter/WinAPIAPI.java
@@ -1,14 +1,20 @@
 package PixelHunter;
 
 
+import com.sun.jna.Callback;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.ptr.PointerByReference;
 
+import javax.swing.*;
+import javax.swing.Timer;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 
@@ -38,6 +44,10 @@ public class WinAPIAPI
 		public static native Pointer OpenProcess(int dwDesiredAccess, boolean bInheritHandle, Pointer pointer);
 	}
 
+    interface MyListener extends WinDef.StdCallCallback
+    {
+        WinDef.LRESULT callback(WinDef.HWND hWnd, int uMsg, WinDef.WPARAM uParam, WinDef.LPARAM lParam);
+    }
 
 	public static class User32DLL
 	{
@@ -50,6 +60,9 @@ public class WinAPIAPI
 		public static native WinDef.HWND GetForegroundWindow();
 
 		public static native int GetWindowTextW(WinDef.HWND hWnd, char[] lpString, int nMaxCount);
+
+        public static native int SetWindowLong(WinDef.HWND hWnd, int nIndex, Callback callback);
+
 	}
 
 	public static void setWindowPos(WinDef.HWND hwnd, int x, int y, int w, int h)
@@ -60,28 +73,52 @@ public class WinAPIAPI
 
 	public static void showMessage(String s)
 	{//REDO!!
+        /*
 		System.out.println("Non-timed out message window: " + s);
 		try {
 			System.in.read();
 		} catch (IOException e) {
 			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
 		}
+		*/
+       InfoFrame frame= new InfoFrame(s);
+       frame.setVisible(true);
+       frame.pack();
 	}
 
 	public static void showMessage(String s, int t)
-	{//REDO
-		System.out.println("Timed out message window: " + s);
-		try {
-			TimeUnit.SECONDS.sleep(t);
-		} catch (InterruptedException e) {
-			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-			WinAPIAPI.showMessage("WARNING: some stupid caused interruption of sleeping. you should find out about the cause!!");
-		}
+	{//REDO todo
+//		System.out.println("Timed out message window: " + s);
+//		try {
+//			TimeUnit.SECONDS.sleep(t);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//			WinAPIAPI.showMessage("WARNING: some stupid caused interruption of sleeping. you should find out about the cause!!");
+//		}
+        final InfoFrame frame= new InfoFrame(s);
+        frame.setVisible(true);
+        frame.pack();
+
+        java.util.Timer timer = new java.util.Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                frame.dispose();
+            }
+        }, 5*1000);
 	}
 
 	public static void toolTip(String s, int x, int y)
-	{    //REDO
-		System.out.println(s);
+	{    //REDO	todo
+		//System.out.println(s);
+        final ToolTip tt= new ToolTip(s, new Point(x,y));
+        java.util.Timer timer = new java.util.Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                tt.dispose();
+            }
+        }, 5*1000);
 	}
 
 	public static Point getMousePos()
