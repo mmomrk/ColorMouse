e177f14b9eb8afe376872ab984cde79d4f803891
mmomrk <mr.ak-47@rambler.ru>
2013-09-04 01:03:26 -0700

    added character ID's

--- src/PixelHunter/GroupedVariables.java
+++ src/PixelHunter/GroupedVariables.java
@@ -45,11 +45,21 @@ public class GroupedVariables
 		//refactor all these were static.note this
 		public static final int ID_PET;
 		public static final int ID_TARGET;
+		public static final int ID_DefaultCharacter;
+		public static final int ID_Warcryer;
+		public static final int ID_Spoiler;
+		public static final int ID_Prophet;
+		public static final int ID_Warlord;
+		public static final int ID_Swordsinger;
+		public static final int ID_Bladedancer;
+		public static final int ID_Warlock;
 
 		public static final Color SECONDARY_LIVING_CREATURE_HP_COLOR;
 		public static final Color PARTY_MEMBERS_PET_HP_COLOR;
 		public static final Color CHAT_COLOR_PARTY;
 		public static final Color CHAT_COLOR_PRIVATE;
+
+//		public static final Point[] WINDOWS_POSITIONS = {new Point(-8, -25), new Point(508, -25)};	//top-left points of window 1 and 2//todo make sure it is not used
 
 		public ProjectConstants()
 		{
@@ -61,15 +71,13 @@ public class GroupedVariables
 
 		private void main()
 		{
-
-			System.out.println("main of grouped variables" + ID_PET + " " + ID_TARGET);
+			logger.trace("GroupedVariables.main. congratulations");
 		}
 
 		static {
 			Properties properties = new Properties();
 			try {
 				properties.load(new FileInputStream("resources/inputData"));
-//				properties.load(GroupedVariables.ProjectConstants.class.getClassLoader().getResourceAsStream("inputData"));
 			} catch (IOException e) {
 				logger.error("Failed to read input configuration file", e);
 				throw new Error("Failed to read configuration", e);
@@ -77,6 +85,16 @@ public class GroupedVariables
 
 			ID_PET = Integer.parseInt(properties.get("ID_pet").toString());
 			ID_TARGET = Integer.parseInt(properties.get("ID_target").toString());
+
+			ID_DefaultCharacter = Integer.parseInt(properties.get("ID_DefaultCharacter").toString());
+			ID_Warcryer  = Integer.parseInt(properties.get("ID_Warcryer").toString());
+			ID_Spoiler   = Integer.parseInt(properties.get("ID_Spoiler").toString());
+			ID_Prophet   = Integer.parseInt(properties.get("ID_Prophet").toString());
+			ID_Warlord   = Integer.parseInt(properties.get("ID_Warlord").toString());
+			ID_Swordsinger = Integer.parseInt(properties.get("ID_Swordsinger").toString());
+			ID_Bladedancer   = Integer.parseInt(properties.get("ID_Bladedancer").toString());
+			ID_Warlock   = Integer.parseInt(properties.get("ID_Warlock").toString());
+
 
 			int red, green, blue;
 			red = Integer.parseInt(properties.get("PetTargetPartyHpColor_red").toString());
