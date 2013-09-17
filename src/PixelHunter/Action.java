7152dce261cbdbf1a48a1d655d9b886788615c10
mmomrk <mr.ak-47@rambler.ru>
2013-09-16 17:16:32 -0700

    base for all scheduled actions

---
+++ src/PixelHunter/Action.java
@@ -0,0 +1,89 @@
+package PixelHunter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * User: mrk
+ * Date: 9/9/13; Time: 5:15 AM
+ */
+abstract class Action
+{
+	private static final Logger logger = LoggerFactory.getLogger(Action.class);
+
+	private static int currentID = 1;
+	protected final int iD;
+
+	protected boolean
+	isBuff,
+	isPvE,
+	isMessageReact,
+	isHomeRun;
+//	protected L2Window l2Window;
+	protected int      priority;    //pve-100;	homerun-200; message-300; buff-350
+
+	public abstract void perform();    //returns macro delay
+
+	public boolean isBuff()
+	{
+		return isBuff;
+	}
+
+	public boolean isPvE()
+	{
+		return isPvE;
+	}
+
+	public boolean isMessageReact()
+	{
+		return isMessageReact;
+	}
+
+	public boolean isHomeRun()
+	{
+		return isHomeRun;
+	}
+
+
+	public int getID()
+	{
+		return this.iD;
+	}
+
+	public int getPriority()
+	{
+		return this.priority;
+	}
+
+
+	protected 	Action()	//watch it!!! todo<-l2window
+	{
+		this.iD = currentID++;
+		if (currentID == Integer.MAX_VALUE) {
+			currentID = 1;
+			WinAPIAPI.showMessage("ID has reached MAX VALUE. unexpected things may happen");
+		}
+//		this.l2Window = l2Window;
+	}
+
+	public static class ActionPriorityComparator implements Comparator<Action>
+	{
+		@Override
+		public int compare(Action chatMessage1, Action chatMessage2)
+		{
+			if (chatMessage1.getPriority() < chatMessage2.getPriority()) {
+				return 1;
+			} else if (chatMessage1.getPriority() > chatMessage2.getPriority()) {
+				return -1;
+			} else {
+				if (chatMessage1.iD > chatMessage2.iD) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+		}
+	}
+}
